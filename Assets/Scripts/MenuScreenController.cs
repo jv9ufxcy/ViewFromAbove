3e3b1ee8e2863cde112edb221707cecaaddd4869
Jaire Vaughn <Jaire.Vaughn@iam.local>
2019-04-18 21:25:13 -0500

    Setup Quiz Scene UI
    Need to make quiz interactable
    Add Timer to Main Game

---
+++ Assets/Scripts/MenuScreenController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuScreenController : MonoBehaviour
+{
+    [SerializeField]
+    private string gameSceneName = "SampleScene", quizSceneName = "QuizScene", menuSceneName = "MenuScene";
+    public void StartGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+    public void StartQuiz()
+    {
+        SceneManager.LoadScene(quizSceneName);
+    }
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(menuSceneName);
+    }
+}
